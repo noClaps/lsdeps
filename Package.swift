680174351cc6124ed8e6551ed27fa04a01ebe142
noClaps <04plugs-bios@icloud.com>
2024-09-18 09:42:30 +0100

    style: Format Swift files

--- Package.swift
+++ Package.swift
@@ -4,21 +4,24 @@
 import PackageDescription
 
 let package = Package(
-  name: "lsdeps",
-  platforms: [
-    .macOS(.v12)
-  ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0")
-  ],
-  targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
-    .executableTarget(
-      name: "lsdeps",
-      dependencies: [
-        .product(name: "ArgumentParser", package: "swift-argument-parser")
-      ]
-    )
-  ]
+    name: "lsdeps",
+    platforms: [
+        .macOS(.v12)
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git",
+            from: "1.5.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "lsdeps",
+            dependencies: [
+                .product(
+                    name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        )
+    ]
 )
--- Sources/lsdeps.swift
+++ Sources/lsdeps.swift
@@ -2,123 +2,133 @@ import ArgumentParser
 import Foundation
 
 struct Package: Decodable {
-  let dependencies: [String: String]?
-  let peerDependencies: [String: String]?
-  let optionalDependencies: [String: String]?
+    let dependencies: [String: String]?
+    let peerDependencies: [String: String]?
+    let optionalDependencies: [String: String]?
 }
 
 func fetch(url: String) async -> Package? {
-  guard let urlObject = URL(string: url) else {
-    print("Error parsing URL: \(url)")
-    return nil
-  }
+    guard let urlObject = URL(string: url) else {
+        print("Error parsing URL: \(url)")
+        return nil
+    }
 
-  guard let (data, _) = try? await URLSession.shared.data(from: urlObject) else {
-    print("Error parsing JSON")
-    return nil
-  }
+    guard let (data, _) = try? await URLSession.shared.data(from: urlObject)
+    else {
+        print("Error parsing JSON")
+        return nil
+    }
 
-  guard let package = try? JSONDecoder().decode(Package.self, from: data) else {
-    print("Error decoding data: \(data)")
-    return nil
-  }
+    guard let package = try? JSONDecoder().decode(Package.self, from: data)
+    else {
+        print("Error decoding data: \(data)")
+        return nil
+    }
 
-  return package
+    return package
 }
 
-func getDeps(packageName: String, skipPeer: Bool, skipOptional: Bool) async -> [String]? {
-  var deps: [String] = []
-  guard let packageData = await fetch(url: "https://registry.npmjs.com/\(packageName)/latest")
-  else {
-    print("Error fetching dependencies for package \(packageName)")
-    return nil
-  }
+func getDeps(packageName: String, skipPeer: Bool, skipOptional: Bool) async
+    -> [String]?
+{
+    var deps: [String] = []
+    guard
+        let packageData = await fetch(
+            url: "https://registry.npmjs.com/\(packageName)/latest")
+    else {
+        print("Error fetching dependencies for package \(packageName)")
+        return nil
+    }
 
-  if let dependencies = packageData.dependencies {
-    for dep in dependencies.keys {
-      if !deps.contains(dep) {
-        deps.append(dep)
-      }
+    if let dependencies = packageData.dependencies {
+        for dep in dependencies.keys {
+            if !deps.contains(dep) {
+                deps.append(dep)
+            }
+        }
     }
-  }
-  if !skipPeer, let dependencies = packageData.peerDependencies {
-    for dep in dependencies.keys {
-      if !deps.contains(dep) {
-        deps.append(dep)
-      }
+    if !skipPeer, let dependencies = packageData.peerDependencies {
+        for dep in dependencies.keys {
+            if !deps.contains(dep) {
+                deps.append(dep)
+            }
+        }
     }
-  }
-  if !skipOptional, let dependencies = packageData.optionalDependencies {
-    for dep in dependencies.keys {
-      if !deps.contains(dep) {
-        deps.append(dep)
-      }
+    if !skipOptional, let dependencies = packageData.optionalDependencies {
+        for dep in dependencies.keys {
+            if !deps.contains(dep) {
+                deps.append(dep)
+            }
+        }
     }
-  }
 
-  return deps
+    return deps
 }
 
 @main
 struct lsdeps: AsyncParsableCommand {
-  @Argument(help: "The npm package to count dependencies for")
-  var package: String
+    @Argument(help: "The npm package to count dependencies for")
+    var package: String
 
-  @Flag(
-    name: [.customShort("p", allowingJoined: true), .long], help: "Skip counting peer dependencies")
-  var skipPeer = false
+    @Flag(
+        name: [.customShort("p", allowingJoined: true), .long],
+        help: "Skip counting peer dependencies")
+    var skipPeer = false
 
-  @Flag(
-    name: [.customShort("o", allowingJoined: true), .long],
-    help: "Skip counting optional dependencies")
-  var skipOptional = false
+    @Flag(
+        name: [.customShort("o", allowingJoined: true), .long],
+        help: "Skip counting optional dependencies")
+    var skipOptional = false
 
-  @Flag(name: .long, help: "Hide the \"Fetching dependencies for...\" messages")
-  var silent = false
+    @Flag(
+        name: .long, help: "Hide the \"Fetching dependencies for...\" messages")
+    var silent = false
 
-  mutating func run() async throws {
-    if !silent {
-      print("Fetching dependencies for \(package)")
-    }
-
-    guard
-      var depSet = await getDeps(
-        packageName: package, skipPeer: skipPeer, skipOptional: skipOptional)
-    else {
-      return
-    }
-
-    var i = 0
-    while i != depSet.count {
-      let setPackage = depSet[i]
-
-      if !silent {
-        print("Fetching dependencies for \(setPackage)")
-      }
-
-      guard
-        let deps = await getDeps(
-          packageName: setPackage, skipPeer: skipPeer, skipOptional: skipOptional)
-      else {
-        continue
-      }
-      for dep in deps {
-        if !depSet.contains(dep) {
-          depSet.append(dep)
+    mutating func run() async throws {
+        if !silent {
+            print("Fetching dependencies for \(package)")
         }
-      }
-      i += 1
+
+        guard
+            var depSet = await getDeps(
+                packageName: package, skipPeer: skipPeer,
+                skipOptional: skipOptional)
+        else {
+            return
+        }
+
+        var i = 0
+        while i != depSet.count {
+            let setPackage = depSet[i]
+
+            if !silent {
+                print("Fetching dependencies for \(setPackage)")
+            }
+
+            guard
+                let deps = await getDeps(
+                    packageName: setPackage, skipPeer: skipPeer,
+                    skipOptional: skipOptional)
+            else {
+                continue
+            }
+            for dep in deps {
+                if !depSet.contains(dep) {
+                    depSet.append(dep)
+                }
+            }
+            i += 1
+        }
+
+        let depsCount = depSet.count
+
+        print(
+            """
+
+            Name: \(package)
+            URL: https://npmjs.com/package/\(package)
+            Dependency count: \(depsCount)
+
+            """)
     }
-
-    let depsCount = depSet.count
-
-    print(
-      """
-
-      Name: \(package)
-      URL: https://npmjs.com/package/\(package)
-      Dependency count: \(depsCount)
-
-      """)
-  }
 }

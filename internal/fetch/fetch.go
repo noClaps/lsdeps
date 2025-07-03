940fc13b3d9197716703317aea6e316e41da5f4e
noClaps <git@zerolimits.dev>
2025-07-03 15:04:32 +0100

    chore: Use shared HTTP client and close response body

--- internal/fetch/fetch.go
+++ internal/fetch/fetch.go
@@ -3,13 +3,25 @@ package fetch
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
+var client = &http.Client{
+	Timeout: 10 * time.Second,
+	Transport: &http.Transport{
+		MaxIdleConns:        100,
+		MaxIdleConnsPerHost: 10,
+		IdleConnTimeout:     90 * time.Second,
+		DisableCompression:  false, // Enable gzip compression
+	},
+}
+
 func Fetch[T any](url string) (*T, error) {
-	res, err := http.Get(url)
+	res, err := client.Get(url)
 	if err != nil {
 		return nil, err
 	}
+	defer res.Body.Close()
 
 	pkg := new(T)
 	err = json.NewDecoder(res.Body).Decode(pkg)
